ea2919e4f99cf5dfc560695537b9157fb9b7757a
Aashish-18 <aashishjha095@gmail.cpm>
2024-04-26 16:16:05 +0530

    corrected

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,21 +1,30 @@
 import { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
 
-const NewsBoard=({category})=>{
-    const[article,setArticle]=useState([]);
-    useEffect(()=>{
-        let url=`https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=c796809895aa4c2a90c7965bacdd8f37`
-        fetch(url).then((response)=>response.json()).then((data)=>setArticle(data.articles)).catch((error) => console.error('Error:', error));
-    },[category])
+const NewsBoard = ({ category }) => {
+    const [articles, setArticles] = useState([]);
 
-    return(
+    useEffect(() => {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=c796809895aa4c2a90c7965bacdd8f37`;
+
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => setArticles(data.articles))
+            .catch((error) => console.error('Error:', error));
+    }, [category]);
+
+    return (
         <div>
-        <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
-        {article.map((news,index)=>{
-            return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
-        })}
+            <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
+            {articles.length > 0 ? (
+                articles.map((news, index) => (
+                    <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />
+                ))
+            ) : (
+                <p>Loading...</p>
+            )}
         </div>
-        
-    )
-}
+    );
+};
+
 export default NewsBoard;
